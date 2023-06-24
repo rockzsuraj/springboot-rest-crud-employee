409153d1b4898a7b867a297da69e3ddfb26c3590
Suraj <skumar@dxlg.com>
2023-06-24 17:02:54 +0530

    with spring-data-jpa & spring-data-rest.

---
+++ src/main/java/com/learn/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.learn.springboot.cruddemo.dao;
+
+import com.learn.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+}
