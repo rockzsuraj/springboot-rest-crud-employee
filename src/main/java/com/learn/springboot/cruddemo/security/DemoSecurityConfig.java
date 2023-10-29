9b88284c1b5657fa559f9750fe309598ac04bcae
skumarz11 <skumar@dxlg.com>
2023-10-29 14:08:56 +0530

    spring api basic security with jdbc

--- src/main/java/com/learn/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/learn/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -8,31 +8,36 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    // add support for JDBC --- no more hardcoded users -)
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource datasource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(datasource);
 
-        UserDetails aman = User.builder().username("aman").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails sachin = User.builder().username("sachin").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
-        UserDetails suraj = User.builder().username("suraj").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
 
-        return new InMemoryUserDetailsManager(aman, sachin, suraj);
-
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+                return jdbcUserDetailsManager;
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer ->
-                configurer
-                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+        http.authorizeHttpRequests(configurer -> configurer.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE").requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE").requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER").requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER").requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
         // use HTTP Basic authentication
         http.httpBasic(Customizer.withDefaults());
@@ -43,4 +48,16 @@ public class DemoSecurityConfig {
 
         return http.build();
     }
+
+/*    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+
+        UserDetails aman = User.builder().username("aman").password("{noop}test123").roles("EMPLOYEE").build();
+        UserDetails sachin = User.builder().username("sachin").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
+        UserDetails suraj = User.builder().username("suraj").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
+
+        return new InMemoryUserDetailsManager(aman, sachin, suraj);
+
+    }
+*/
 }
